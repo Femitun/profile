8ae0091a2979c81283f317ff74e441e482820c6f
Femitun <bbabatunde.2202631@stu.cu.edu.ng>
2024-11-09 07:23:57 +0100

    Create mail.js

---
+++ mail.js
@@ -0,0 +1,34 @@
+"use strict mode";
+//Public Key
+emailjs.init("7pJzwqIbYoMqNt7Qz");
+
+// Function to send email
+function sendEmail() {
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  // EmailJS parameters
+  const templateParams = {
+    user_email: email,
+    message: message,
+  };
+
+  emailjs
+    .send("service_ysg5dln", "template_b3spvy2", templateParams)
+    .then((response) => {
+      console.log("Email sent successfully:", response.status, response.text);
+      showToast();
+    })
+    .catch((error) => {
+      console.error("Email sending error:", error);
+    });
+}
+
+// Function to show toast notification
+function showToast() {
+  const toast = document.getElementById("success");
+  toast.classList.add("show");
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 3000); // Duration of toast visibility in ms
+}
